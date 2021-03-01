821a1f4ec401379b070c3401e42a80607db5661b
helong-y <75062958+helong-y@users.noreply.github.com>
2021-03-01 09:30:06 +0800

    第二次上传，测试验证码，修改host

--- scaffolding/src/views/test.vue
+++ scaffolding/src/views/test.vue
@@ -1,25 +1,20 @@
 <template>
   <div>
-    <div class="block">
-      1324564564
-    </div>
+    <!-- <div class="layui-input-block">
+                        <input class="captchaInp" type="text" name="">
+                        <img src="{{_HOST_}}/admin/login/code" alt="看不清？点击刷新" class="captchaPic"
+                            onclick="javascript:this.src='{{_HOST_}}/admin/login/code?mt='+Math.random()">
+ </div> -->
   </div>
 </template>
 <style scoped>
-  .block::after {
-content: '';
-  width: 60%;
-  height: 1px;
-  display: block;
-  margin: 0 auto;
-  border-bottom: 1px solid black;
-}
+
 </style>
 <script>
  
 export default {
   components:{
-    
+  
   }
 }
 </script>
